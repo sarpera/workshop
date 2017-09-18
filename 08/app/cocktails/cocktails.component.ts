95b96d83bfcc013f0e5698ad7077c23dcd37fcfe
Sarper Avcil <sarperavcil@gmail.com>
2017-09-18 19:29:10 +0200

    cleanup

--- 08/app/cocktails/cocktails.component.ts
+++ 08/app/cocktails/cocktails.component.ts
@@ -1,24 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ICocktail } from '../cocktail';
 import { CocktailService } from '../cocktail.service';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
     selector   : 'np-cocktails',
     templateUrl: './cocktails.component.html',
-    styleUrls  : ['./cocktails.component.scss'],
-    animations : [
-        trigger('flyInOut', [
-            state('in', style({ transform: 'translateX(0)' })),
-            transition('void => *', [
-                style({ transform: 'translateX(-100%)' }),
-                animate(300)
-            ]),
-            transition('* => void', [
-                animate(300, style({ transform: 'translateX(100%)' }))
-            ])
-        ])
-    ]
+    styleUrls  : ['./cocktails.component.scss']
 })
 export class CocktailsComponent implements OnInit {
 
